7b22a004f6d0130de927d90003491dc9e6c1ddff
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-02 10:07:08 +0200

    Update setpos.lua

--- hacks/setpos.lua
+++ hacks/setpos.lua
@@ -1,5 +1,10 @@
 coordBase = readPointer(getBaseAddress("GeometryDash.exe")+0x303118, 0x164, 0x220, 0x674)
-newXPos = userInputInt("Enter new XPos (current is "..tostring(readFloat(coordBase))..")")
-newYPos = userInputInt("Enter new YPos (current is "..tostring(readFloat(coordBase+4))..")")
-writeFloat(coordBase, newXPos)
-writeFloat(coordBase+4, newYPos)
+if coordBase == 0 then
+  error = 1
+  errorMessage = "you arent playing any lvl"
+else
+  newXPos = userInputInt("Enter new XPos (current is "..tostring(readFloat(coordBase))..")")
+  newYPos = userInputInt("Enter new YPos (current is "..tostring(readFloat(coordBase+4))..")")
+  writeFloat(coordBase, newXPos)
+  writeFloat(coordBase+4, newYPos)
+end
