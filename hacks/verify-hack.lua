1b621840130a338970a849150673edb98b0225c9
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-03 13:03:52 +0200

    Update verify-hack.lua

--- hacks/verify-hack.lua
+++ hacks/verify-hack.lua
@@ -1,8 +1,6 @@
 base = getBaseAddress("GeometryDash.exe")
-if not (writeHexString(base+0x6D688, "EB")  and
-        writeHexString(base+0x6D5AA, "EB")  and
-        writeHexString(base+0x6D628, "EB")) then
+if not writeHexString(base+0x6D688, "EB") then
   error = 1
-	errorMessage = "Failed to write memory"
+  errorMessage = "Failed to write memory"
 end
 isToggleable = true
