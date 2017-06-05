1ae93411044bff91519b04ae12f482bc18024c9b
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-05 17:28:57 +0200

    Update teleport-hax.lua

--- hacks/teleport-hax.lua
+++ hacks/teleport-hax.lua
@@ -1,7 +1,9 @@
 function background()
-  coordBase = readPointer(getBaseAddress("GeometryDash.exe")+0x303118, 0x164, 0x220, 0x674)
-  if coordBase ~= 0 then
-    newXPos = 1000000
-    writeFloat(coordBase, newXPos)
+  while true do
+    coordBase = readPointer(getBaseAddress("GeometryDash.exe")+0x303118, 0x164, 0x220, 0x674)
+    if coordBase ~= 0 then
+      newXPos = 1000000
+      writeFloat(coordBase, newXPos)
+    end
   end
 end
