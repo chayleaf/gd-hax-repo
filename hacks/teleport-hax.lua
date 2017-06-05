d94d16db17a319d7f2498db8a6d8e10202315d93
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-05 17:24:47 +0200

    Create teleport-hax.lua
    testing background stuff

---
+++ hacks/teleport-hax.lua
@@ -0,0 +1,7 @@
+function background()
+  coordBase = readPointer(getBaseAddress("GeometryDash.exe")+0x303118, 0x164, 0x220, 0x674)
+  if coordBase ~= 0 then
+    newXPos = 1000000
+    writeFloat(coordBase, newXPos)
+  end
+end
