b6396ade82d3b2ab37c85c2004729ff452d76651
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-09 22:32:07 +0200

    Create noclip-v2.lua

--- hacks/noclip-v2.lua
+++ hacks/noclip-v2.lua
@@ -1,13 +1,13 @@
 --the new syntax
-mem = MemEditor:MemEditor()
+mem = MemEditor()
 
---mem.enableCancel() --allow user canceling the hack
+mem.enableCancel() --allow user canceling the hack
 
---base = mem:getBaseAddress("GeometryDash.exe")
---if not (mem:writeInt(base+0x1F36C5, 0x0005CCE9) and 
---	mem:writeInt(base+0x1F36C9, 0x9000, 2)) then
---	error = 1
---	message = "Failed to write memory"
---else
---	message "Dont forget to enable antikick :D"
---end
+base = mem:getBaseAddress("GeometryDash.exe")
+if not (mem:writeInt(base+0x1F36C5, 0x0005CCE9) and 
+	mem:writeInt(base+0x1F36C9, 0x9000, 2)) then
+	error = 1
+	message = "Failed to write memory"
+else
+	message "Dont forget to enable antikick :D"
+end
