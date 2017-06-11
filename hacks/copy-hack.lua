a968a5badd9c6586253dce71bf1f8b0935270cd3
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-11 18:16:40 +0200

    Create copy-hack.lua

--- hacks/copy-hack.lua
+++ hacks/copy-hack.lua
@@ -1,6 +1,7 @@
-base = getBaseAddress("GeometryDash.exe")
-if not writeHexString(base+0x162DA6, "E91E01000090") then
+mem = MemEditor("GeometryDash.exe")
+mem:enableCancel()
+base = mem:getBaseAddress("GeometryDash.exe")
+if not mem:writeHexString(base+0x162DA6, "E91E01000090") then
 	error = 1
-	errorMessage = "Failed to write memory"
+	message = "Failed to write memory"
 end
-isToggleable = true
