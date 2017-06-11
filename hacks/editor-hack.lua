aadf64433a821cc610c140f80e21802d5eb6a56d
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-11 18:16:00 +0200

    Create editor-hack.lua

--- hacks/editor-hack.lua
+++ hacks/editor-hack.lua
@@ -1,8 +1,9 @@
-base = getBaseAddress("GeometryDash.exe")
-if not (writeHexString(base+0x907A3, "909090909090") and 
-	writeHexString(base+0x1CF7B2, "9090") and
-  writeHexString(base+0x1E5B40, "EB")) then
+mem = MemEditor("GeometryDash.exe")
+mem:enableCancel()
+base = mem:getBaseAddress("GeometryDash.exe")
+if not (mem:writeHexString(base+0x907A3, "909090909090") and 
+	mem:writeHexString(base+0x1CF7B2, "9090") and
+  	mem:writeHexString(base+0x1E5B40, "EB")) then
 	error = 1
-	errorMessage = "Failed to write memory"
+	message = "Failed to write memory"
 end
-isToggleable = true
