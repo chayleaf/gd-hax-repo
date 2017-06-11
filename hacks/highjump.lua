9f6e01434642d8601968e4117f82a8e0e250aba7
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-11 18:16:16 +0200

    Create highjump.lua

--- hacks/highjump.lua
+++ hacks/highjump.lua
@@ -2,5 +2,5 @@ mem = MemEditor("GeometryDash.exe")
 mem:enableCancel()
 if not mem:writeInt(mem:getBaseAddress("GeometryDash.exe")+1916058, 271) then
   error = 1
-  errorMessage = "Failed to write memory"
+  message = "Failed to write memory"
 end
