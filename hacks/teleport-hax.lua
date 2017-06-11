c41a2ae0f9b583ca9f2dab4ca0b10ab56c940f7b
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-11 18:20:18 +0200

    Create teleport-hax.lua

--- hacks/teleport-hax.lua
+++ hacks/teleport-hax.lua
@@ -1,9 +1,11 @@
+mem = MemEditor("GeometryDash.exe")
+mem:enableCancel()
 function background()
   while true do
-    coordBase = readPointer(getBaseAddress("GeometryDash.exe")+0x303118, 0x164, 0x220, 0x674)
+    coordBase = mem:readPointer(getBaseAddress("GeometryDash.exe")+0x303118, 0x164, 0x220, 0x674)
     if coordBase ~= 0 then
       newXPos = 1000000
-      writeFloat(coordBase, newXPos)
+      mem:writeFloat(coordBase, newXPos)
     end
   end
 end
