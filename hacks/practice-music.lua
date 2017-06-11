61191d313ccea542dd649f5c3f3ed4c3f254917e
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-11 18:17:18 +0200

    Create practice-music.lua

--- hacks/practice-music.lua
+++ hacks/practice-music.lua
@@ -1,9 +1,10 @@
-base = getBaseAddress("GeometryDash.exe")
-if not (writeHexString(base+0x2429E, "EB") and
-        writeHexString(base+0x1F61F4, "3F") and
-        writeHexString(base+0x1F59C5, "909090909090") and
-        writeHexString(base+0x1F6752, "909090909090")) then
+mem = MemEditor("GeometryDash.exe")
+mem:enableCancel()
+base = mem:getBaseAddress("GeometryDash.exe")
+if not (mem:writeHexString(base+0x2429E, "EB") and
+        mem:writeHexString(base+0x1F61F4, "3F") and
+        mem:writeHexString(base+0x1F59C5, "909090909090") and
+        mem:writeHexString(base+0x1F6752, "909090909090")) then
         error = 1
-        errorMessage = "Failed to write memory"
+        message = "Failed to write memory"
 end
-isToggleable = true
