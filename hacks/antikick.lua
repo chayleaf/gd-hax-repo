709a1bced93123ceb1d54a3d0434fa123f803c54
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-11 18:22:07 +0200

    Create antikick.lua

--- hacks/antikick.lua
+++ hacks/antikick.lua
@@ -1,12 +1,13 @@
-base = getBaseAddress("GeometryDash.exe")
-if not (writeInt(base+0x1E6967, 0x9090, 2) and 
-	writeInt(base+0x1E6ABE, 0xEB, 1) and
-	writeInt(base+0x1E6CC3, 0xEB, 1) and
-	writeHexString(base+0x1E6EF1, "909090909090") and
-	writeHexString(base+0x1E6EFE, "909090909090") and
-	writeHexString(base+0x1EBBF4, "E94A01000090") and
-	writeHexString(base+0x1F56BD, "E99D00000090")) then
+mem = MemEditor("GeometryDash.exe")
+mem:enableCancel()
+base = mem:getBaseAddress("GeometryDash.exe")
+if not (mem:writeInt(base+0x1E6967, 0x9090, 2) and 
+	mem:writeInt(base+0x1E6ABE, 0xEB, 1) and
+	mem:writeInt(base+0x1E6CC3, 0xEB, 1) and
+	mem:writeHexString(base+0x1E6EF1, "909090909090") and
+	mem:writeHexString(base+0x1E6EFE, "909090909090") and
+	mem:writeHexString(base+0x1EBBF4, "E94A01000090") and
+	mem:writeHexString(base+0x1F56BD, "E99D00000090")) then
 	error = 1
-	errorMessage = "Failed to write memory"
+	message = "Failed to write memory"
 end
-isToggleable = true
