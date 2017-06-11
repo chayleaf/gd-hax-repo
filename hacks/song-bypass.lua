07384f6e9ac3f344b5b24f34f3f1117a6bd13bef
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-11 18:19:33 +0200

    Create song-bypass.lua

--- hacks/song-bypass.lua
+++ hacks/song-bypass.lua
@@ -1,10 +1,12 @@
-base = getBaseAddress("GeometryDash.exe")
-if not writeString(base+0x2B15BC, "http://teamhax.altervista.org/rwh/getGJSongInfo.php") then --more addresses coming
+mem = MemEditor("GeometryDash.exe")
+mem:enableCancel()
+base = mem:getBaseAddress("GeometryDash.exe")
+if not mem:writeString(base+0x2B15BC, "http://teamhax.altervista.org/rwh/getGJSongInfo.php") then --more addresses coming
   error = 1
-  errorMessage = "Failed to write memory"
-  --[[answer = userInputString("Do you want to download new official songs (From GDW and GDMD)?"
+  message = "Failed to write memory"
+else
+  answer = mem:userInputString("Do you want to download new official songs (From GDW and GDMD)? (yes/no)"
   if answer:sub(0,0) == "y" or answer:sub(0,0) == "Y" then
     os.execute("start http://teamhax.altervista.org/rwh/songs.zip")
-  end]]
+  end
 end
-isToggleable = true
