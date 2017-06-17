b9371cbac9348efcb0dd7b3288a0cbd6791c59d3
Ivan Pavluk <pavlukivan@outlook.com>
2017-06-17 15:15:40 +0200

    Update song-bypass.lua

--- hacks/song-bypass.lua
+++ hacks/song-bypass.lua
@@ -5,7 +5,7 @@ if not mem:writeString(base+0x2B15BC, "http://teamhax.altervista.org/rwh/getGJSo
   error = 1
   message = "Failed to write memory"
 else
-  answer = mem:userInputString("Do you want to download new official songs (From GDW and GDMD)? (yes/no)"
+  answer = mem:userInputString("Do you want to download new official songs (From GDW and GDMD)? (yes/no)")
   if answer:sub(0,0) == "y" or answer:sub(0,0) == "Y" then
     os.execute("start http://teamhax.altervista.org/rwh/songs.zip")
   end
